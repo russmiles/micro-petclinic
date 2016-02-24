f2224faf37ab6eb241efefb7a8017784bb22870f
Russ Miles <russ@russmiles.com>
2016-02-24 13:07:46 +0000

    Added first Spring initializr version of front-end web ui gateway service.

---
+++ services/web-ui-gateway-service/src/main/java/com/russmiles/antifragilesoftware/PetclinicWebUiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.russmiles.antifragilesoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicWebUiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclinicWebUiGatewayApplication.class, args);
+	}
+}
